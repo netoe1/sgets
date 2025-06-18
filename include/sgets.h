be68322ddb5a01c8ad15cb14ec1ad2640fcd399e
netoe1 <elynetobr@outlook.com>
2025-06-18 20:25:32 -0300

    sgets.h update

--- include/sgets.h
+++ include/sgets.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void clearBuffers();                            // A way to clear buffers using fseek() in a binary way.
-void sgets(char *s, size_t size, FILE *stream); // Implementation of sgets()
-
+void clearBuffers();                                 // A way to clear buffers using fseek() in a binary way.
+void sgets_file(char *s, size_t size, FILE *stream); // Implementation of sgets()!
+void sgets(char *s, size_t size);                    // sgets for stdin!
 #endif
--- src/sgets.c
+++ src/sgets.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/sgets.h"
 void clearBuffers()
 {
     fseek(stdin, 0, SEEK_END);
@@ -15,8 +16,36 @@ static void __validstr(const char *s)
         fprintf(stderr, "Error: Invalid str, risk of memory leaks!");
     }
 }
-void sgets(char *s, size_t size, FILE *stream)
+void sgets_file(char *s, size_t size, FILE *stream)
 {
+
+    __validstr(s);
+    if (stream == NULL)
+    {
+        fprintf(stderr, "Error: File stream shouldn't be NULL!\n");
+        return;
+    }
+
+    if (size <= 0)
+    {
+        fprintf(stderr, "Error: Effective size cannot be less than 0!\n");
+        return;
+    }
+
+    if (size > sizeof(s))
+    {
+        fprintf(stderr, "Effective size cannot be greater than real size ones!\n");
+        return;
+    }
+
+    setbuf(stdin, NULL);
+    fgets(s, size, stream);
+    s[strcspn(s, "\n")] = '\0';
+}
+
+void sgets(char *s, size_t size)
+{
+    FILE *stream = stdin;
 
     __validstr(s);
     if (stream == NULL)
